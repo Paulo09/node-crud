cef0d5264afe3d449df6570dae4e8c548dce52ac
Paulo09 <paullocasttro@hotmail.com>
2023-09-21 19:39:33 -0300

    Add Host and Port

--- NODE-CRUD/server.js
+++ NODE-CRUD/server.js
@@ -1,3 +1,20 @@
 import {fastify} from "fastify";
+import { request } from "http";
 
 const server = fastify();
+
+const host = 'localhost'
+const port = 5000
+
+// Declare a route
+server.get('/', async (req, res) => {
+
+    res.send('Servidor Rodando!!!');
+})
+
+server.listen({
+    host,
+    port   
+})
+.then(() => console.log(`servidor rodando em http://${host}:${port}`))
+.catch(err => console.log('Erro ao Subir o Servidor:',err))

2d902f050e43b77a81a654ca0f2b51c2d654b317
Paulo09 <paullocasttro@hotmail.com>
2023-09-21 21:04:05 -0300

    add objto Id

--- NODE-CRUD/db-list.js
+++ NODE-CRUD/db-list.js
@@ -5,7 +5,12 @@ export class DataBaseTemp{
     produtos = []
 
     adicionar = (produto) => {      
-        console.log('Produto',produto)
+        console.log('Produto_antes',produto)
+        
+        // Add propriedade com ID Randomico
+        produto.id = randomUUID();
+
+        console.log('Produto_depois',produto)
         return this.produtos.push(produto)
     }
 
